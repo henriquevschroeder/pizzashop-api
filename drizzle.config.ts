073ac8dd246dfb60823775b1ba54e9d319d9f24e
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2024-04-26 20:15:32 -0300

    feat: add support for env file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from 'drizzle-kit'
+import { env } from './src/env'
 
 export default {
   schema: './src/db/schema/index.ts',
   out: './src/db/migrations',
   driver: 'pg',
   dbCredentials: {
-    connectionString: 'postgresql://docker:docker@localhost:5432/pizzashop'
+    connectionString: env.DATABASE_URL
   }
 } satisfies Config
--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,13 +1,14 @@
 import postgres from 'postgres'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
+import { env } from '../env'
+import chalk from 'chalk'
 
-const connection =
-  postgres('postgresql://docker:docker@localhost:5432/pizzashop', { max: 1 })
-
+const connection = postgres(env.DATABASE_URL, { max: 1 })
 const db = drizzle(connection)
 
 await migrate(db, { migrationsFolder: './src/db/migrations' })
+console.log(chalk.green('✅ Migrations applied succesfully'))
 await connection.end()
 
 process.exit()
