17d08b7e37912649b1a396c73b664929ce9e2524
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2024-04-26 20:29:45 -0300

    feat: create db seed script

---
+++ src/db/seed.ts
@@ -0,0 +1,64 @@
+import { faker } from '@faker-js/faker'
+import { users, restaurants } from './schema'
+import { db } from './connection'
+import chalk from 'chalk'
+
+/**
+ * Reset Database
+ */
+
+await db.delete(users)
+await db.delete(restaurants)
+
+console.log(chalk.green('✅ Reset database!'))
+
+/**
+ * Create customers
+ */
+
+await db.insert(users).values([
+  {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    role: 'customer'
+  },
+  {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    role: 'customer'
+  }
+])
+
+console.log(chalk.green('✅ Created customers!'))
+
+/**
+ * Create manager
+ */
+
+const [manager] = await db.insert(users).values([
+  {
+    name: faker.person.fullName(),
+    email: 'admin@admin.com',
+    role: 'manager'
+  }
+]).returning({
+  id: users.id
+})
+
+console.log(chalk.green('✅ Created manager!'))
+
+/**
+ * Create restaurant
+ */
+
+await db.insert(restaurants).values([
+  {
+    name: faker.company.name(),
+    description: faker.lorem.paragraph(),
+    managerId: manager.id
+  }
+])
+
+console.log(chalk.greenBright('✅ Database seeded!'))
+
+process.exit()
