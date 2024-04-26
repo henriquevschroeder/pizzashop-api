08e8eb9e1ca2ed61039dfb6c6846c5982436c271
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2024-04-26 19:56:27 -0300

    feat: create db migration script

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,5 +3,8 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './src/db/schema/index.ts',
   out: './src/db/migrations',
-  driver: 'pg'
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: 'postgresql://docker:docker@localhost:5432/pizzashop'
+  }
 } satisfies Config
