974bc6306ca86c550bbb6821e5c8143e2ff9e17a
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2024-04-27 20:28:16 -0300

    feat: add route to receive the authentication link

---
+++ src/http/routes/authenticate-from-link.ts
@@ -0,0 +1,58 @@
+import { Elysia, t } from 'elysia'
+import { db } from '../../db/connection'
+import dayjs from 'dayjs'
+import { auth } from '../auth'
+import { authLinks } from '../../db/schema'
+import { eq } from 'drizzle-orm'
+
+export const authenticateFromLink = new Elysia().use(auth).get(
+  '/auth-links/authenticate',
+  async ({ query, jwt: { sign }, cookie: { authCookie }, set }) => {
+    const { code, redirect } = query
+
+    const authLinkFromCode = await db.query.authLinks.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.code, code)
+      },
+    })
+
+    if (!authLinkFromCode) {
+      throw new Error('Auth link not found.')
+    }
+
+    const daysSinceAuthLinkWasCreated = dayjs().diff(
+      authLinkFromCode.createdAt,
+      'days',
+    )
+
+    if (daysSinceAuthLinkWasCreated > 7) {
+      throw new Error('Auth link expired. Please generate a new one.')
+    }
+
+    const managedRestaurant = await db.query.restaurants.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.managerId, authLinkFromCode.userId)
+      },
+    })
+
+    const jwt = await sign({
+      sub: authLinkFromCode.userId,
+      restaurantId: managedRestaurant?.id,
+    })
+
+    authCookie.value = jwt
+    authCookie.httpOnly = true
+    authCookie.maxAge = 60 * 60 * 24 * 7 // 7 days
+    authCookie.path = '/'
+
+    await db.delete(authLinks).where(eq(authLinks.code, code))
+
+    set.redirect = redirect
+  },
+  {
+    query: t.Object({
+      code: t.String(),
+      redirect: t.String(),
+    }),
+  },
+)
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,12 @@
 import { Elysia } from 'elysia'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
+import { authenticateFromLink } from './routes/authenticate-from-link'
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+const app = new Elysia()
+  .use(registerRestaurant)
+  .use(sendAuthLink)
+  .use(authenticateFromLink)
 
 app.listen(3333, () => {
   console.log('🚀 HTTP Server is running on port 3333')
