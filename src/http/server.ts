44f0b8a55bacbb6957eb4c1f9f28118dd45d252f
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2024-04-28 18:03:19 -0300

    feat: add route to get month revenue

--- src/http/server.ts
+++ src/http/server.ts
@@ -11,6 +11,7 @@ import { cancelOrder } from './routes/cancel-order'
 import { deliverOrder } from './routes/deliver-order'
 import { dispatchOrder } from './routes/dispatch-order'
 import { getOrders } from './routes/get-orders'
+import { getMonthRevenue } from './routes/get-month-revenue'
 
 const app = new Elysia()
   .use(registerRestaurant)
@@ -25,6 +26,7 @@ const app = new Elysia()
   .use(deliverOrder)
   .use(dispatchOrder)
   .use(getOrders)
+  .use(getMonthRevenue)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION':
