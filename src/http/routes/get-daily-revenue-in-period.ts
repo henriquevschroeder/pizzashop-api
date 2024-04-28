9201e9fdaf9a9c1285f1ce01bb7be35d74410936
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2024-04-28 19:59:54 -0300

    feat: add route to get daily revenue in a period

---
+++ src/http/routes/get-daily-revenue-in-period.ts
@@ -0,0 +1,76 @@
+import Elysia, { t } from 'elysia'
+import { auth } from '../auth'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+import { db } from '../../db/connection'
+import { orders } from '../../db/schema'
+import dayjs from 'dayjs'
+import { and, eq, gte, lte, sql, sum } from 'drizzle-orm'
+
+export const getDailyRevenueInPeriod = new Elysia().use(auth).get(
+  '/metrics/daily-revenue-in-period',
+  async ({ getCurrentUser, query, set }) => {
+    const { restaurantId } = await getCurrentUser()
+
+    if (!restaurantId) {
+      throw new UnauthorizedError()
+    }
+
+    const { from, to } = query
+
+    const startDate = from ? dayjs(from) : dayjs().subtract(7, 'days')
+    const endDate = to ? dayjs(to) : from ? startDate.add(7, 'days') : dayjs()
+
+    if (endDate.diff(startDate, 'days') > 7) {
+      set.status = 400
+      return {
+        message: 'You can not list the revenue in a larger period than 7 days',
+      }
+    }
+
+    const revenuePerDay = await db
+      .select({
+        date: sql<string>`TO_CHAR(${orders.createdAt}, 'MM-DD')`,
+        revenue: sum(orders.totalInCents).mapWith(Number),
+      })
+      .from(orders)
+      .where(
+        and(
+          eq(orders.restaurantId, restaurantId),
+          gte(
+            orders.createdAt,
+            startDate
+              .startOf('day')
+              .add(startDate.utcOffset(), 'minutes')
+              .toDate(),
+          ),
+          lte(
+            orders.createdAt,
+            endDate.endOf('day').add(startDate.utcOffset(), 'minutes').toDate(),
+          ),
+        ),
+      )
+      .groupBy(sql`TO_CHAR(${orders.createdAt}, 'MM-DD')`)
+
+    const orderedRevenuePerDay = revenuePerDay.sort((a, b) => {
+      const [monthA, dayA] = a.date.split('-').map(Number)
+      const [monthB, dayB] = b.date.split('-').map(Number)
+
+      if (monthA === monthB) {
+        return dayA - dayB
+      } else {
+        const dateA = new Date(2024, monthA - 1)
+        const dateB = new Date(2024, monthB - 1)
+
+        return dateA.getTime() - dateB.getTime()
+      }
+    })
+
+    return orderedRevenuePerDay
+  },
+  {
+    query: t.Object({
+      from: t.Optional(t.String()),
+      to: t.Optional(t.String()),
+    }),
+  },
+)
